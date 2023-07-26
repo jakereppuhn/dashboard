156800140840dda3d270d1a308ef0adf7a07ddaa
jacobreppuhn <jakereppuhn@gmail.com>
2023-07-25 23:27:26 -0400

    nice ass checkbox

--- client/src/components/inputs/CheckBox.jsx
+++ client/src/components/inputs/CheckBox.jsx
@@ -13,25 +13,22 @@ function Checkbox() {
 				onChange={() => setChecked(!checked)}
 			/>
 			<div
-				className={`w-4 h-4 rounded bg-gray-500 flex items-center justify-center ${
-					checked ? 'bg-blue-600' : 'bg-gray-500'
+				className={`w-[18px] h-[18px] rounded  flex items-center justify-center border border-gray-500 ${
+					checked ? 'text-blue-400 border-blue-500' : 'bg-gray-800 text-gray-500'
 				}`}>
-				{checked && (
-					<svg
-						fill="none"
-						className="text-white"
-						stroke="currentColor"
-						strokeWidth={1.5}
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-						aria-hidden="true">
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M4.5 12.75l6 6 9-13.5"
-						/>
-					</svg>
-				)}
+				<svg
+					fill="none"
+					stroke="currentColor"
+					strokeWidth={3}
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					aria-hidden="true">
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M4.5 12.75l6 6 9-13.5"
+					/>
+				</svg>
 			</div>
 		</label>
 	);
